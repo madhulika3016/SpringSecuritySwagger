b848ab88000bb7044fcb13c45ae0bd4dc49e633f
madhulika3016 <madhulikapa@gmail.com>
2024-04-19 12:12:18 +0530

    commit

---
+++ SpringSecurityJWTAuthDemo/src/main/java/com/security/WebSecurityConfig.java
@@ -0,0 +1,111 @@
+package com.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.security.jwt.AuthEntryPointJwt;
+import com.security.jwt.AuthTokenFilter;
+import com.security.services.UserDetailsServiceImpl;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+@EnableMethodSecurity
+@EnableWebSecurity
+public class WebSecurityConfig //extends WebSecurityConfiguration
+{
+  @Autowired
+  UserDetailsServiceImpl userDetailsService;
+
+  @Autowired
+  private AuthEntryPointJwt unauthorizedHandler;
+
+  @Bean
+  public AuthTokenFilter authenticationJwtTokenFilter() {
+    return new AuthTokenFilter();
+  }
+  
+  @Bean
+  public DaoAuthenticationProvider authenticationProvider() {
+      DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+       
+      authProvider.setUserDetailsService(userDetailsService);
+      authProvider.setPasswordEncoder(passwordEncoder());
+   
+      return authProvider;
+  }
+
+  
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+    return authConfig.getAuthenticationManager();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+
+  
+  private SecurityScheme createAPIKeyScheme() {
+	    return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+	        .bearerFormat("JWT")
+	        .scheme("bearer");
+	}
+  
+  @Bean
+  public OpenAPI openAPI() {
+      return new OpenAPI().addSecurityItem(new SecurityRequirement().
+              addList("Bearer Authentication"))
+          .components(new Components().addSecuritySchemes
+              ("Bearer Authentication", createAPIKeyScheme()))
+          .info(new Info().title("My REST API")
+              .description("Some custom description of API.")
+              .version("1.0").contact(new Contact().name("Madhulika")
+                  .email( "").url(""))
+              .license(new License().name("License of API")
+                  .url("API license URL")));
+  }
+  
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.csrf(csrf -> csrf.disable())
+        .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(auth -> 
+          auth.requestMatchers("/api/**").permitAll()
+              .requestMatchers("/api/**").permitAll()
+              .requestMatchers("/swagger-ui/**", "swagger-ui**", "/v3/api-docs/**", "/v3/api-docs**").permitAll()
+              .anyRequest().authenticated()
+        );
+    
+    http.authenticationProvider(authenticationProvider());
+
+    http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+    
+    return http.build();
+  }
+}
---
+++ SpringSecurityJWTAuthDemo/src/test/java/com/SpringSecurityJwtAuthDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtAuthDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
